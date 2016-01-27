daa08ab47cfbdbe02a890e6aa4e2929c2ae8f538
visitter <ivanzhotev@gmail.com>
2016-01-27 19:53:03 +0200

    Second commit(forgotten file))

---
+++ 2Environmentals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Environmentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowEnvironmentDetails();
+        }
+
+        static void ShowEnvironmentDetails()
+        {   
+            
+            foreach( DictionaryEntry de in Environment.GetEnvironmentVariables())            
+            {                
+                Console.WriteLine(de.Key + " = " + de.Value);
+            }
+            
+            /*second way(using enumerator) of iterating through collection
+            IDictionary id = Environment.GetEnvironmentVariables();
+            IDictionaryEnumerator idn = id.GetEnumerator();
+
+            while (idn.MoveNext())
+            {
+                Console.WriteLine(idn.Key+ " = "+idn.Value);            
+            }
+            */
+            Console.ReadLine();
+        }
+    }
+}
