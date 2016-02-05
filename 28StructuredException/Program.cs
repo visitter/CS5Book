65efc1df5dd277db91cf00ee6d68ce189fcb5971
visitter <ivanzhotev@gmail.com>
2016-02-05 16:49:18 +0200

    28asd

--- 28StructuredException/Program.cs
+++ 28StructuredException/Program.cs
@@ -6,10 +6,112 @@ using System.Threading.Tasks;
 
 namespace _28StructuredException
 {
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            if (on)
+                Console.WriteLine("Jamming...");
+            else
+                Console.WriteLine("Quiet time...");
+        }
+    }
+
+    class Car
+    {
+        // Constant for maximum speed.
+        public const int MaxSpeed = 100;
+        // Car properties.
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        // Is the car still operational?
+        private bool carIsDead;
+        // A car has-a radio.
+        private Radio theMusicBox = new Radio();
+        // Constructors.
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+        // See if Car has overheated.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+
+        // See if Car has overheated.
+        public void AccelerateEx(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    //Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    throw new Exception(String.Format("{0} has overheated", PetName));
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            driveCar();
+            Console.ReadLine(); 
+        }
+
+        static void driveCar()
+        {
+            Console.WriteLine("***** Simple Exception Example *****");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar.AccelerateEx(10);
+                //myCar.Accelerate(10);
+            }
+            catch (Exception e)
+            {                
+                System.Reflection.ParameterInfo[] paramsInfo =  e.TargetSite.GetParameters();
+                String str = "";
+                foreach ( System.Reflection.ParameterInfo item in paramsInfo)
+	            {
+                    str += item;
+	            }
+
+                Console.WriteLine("Exception source: {0}",e.Source);
+                Console.WriteLine(e.TargetSite.MemberType+"->"+e.TargetSite.DeclaringType + "." + e.TargetSite.Name+"("+str+")");
+                Console.WriteLine("Exception message: {0}", e.Message); 
+            }
         }
     }
 }
