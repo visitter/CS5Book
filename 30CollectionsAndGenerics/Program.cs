be44c4a8ced6dedeeb3e3bd34102af99ba3d7b65
visitter <ivanzhotev@gmail.com>
2016-02-18 13:21:30 +0200

    _30home

--- 30CollectionsAndGenerics/Program.cs
+++ 30CollectionsAndGenerics/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _30CollectionsAndGenerics
-{
+{    
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            showMyArrayList();
+        }
+
+        static void showMyArrayList()
+        {
+            ArrayList arrList = new ArrayList();
+            arrList.AddRange(new String[] { "1", "2", "3" });
+            Console.WriteLine("The count of the list is {0}", arrList.Count );
+
+            ArrayList arrInts = new ArrayList();
+            arrInts.AddRange(new int[] { 1, 2, 3, 4 });
+
+            arrList.Add("4");
+            Console.WriteLine("The count of the list is {0}", arrList.Count);
+
+            Console.WriteLine("Listing the items in the list");
+            foreach (var item in arrList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Listing the items in the Ints");
+            foreach (var item in arrInts)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
