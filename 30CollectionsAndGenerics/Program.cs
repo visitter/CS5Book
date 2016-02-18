d0eab9b464493297e141e7c2eb2c2fd416c55be3
visitter <ivanzhotev@gmail.com>
2016-02-18 13:31:50 +0200

    _30work

--- 30CollectionsAndGenerics/Program.cs
+++ 30CollectionsAndGenerics/Program.cs
@@ -10,6 +10,21 @@ namespace _30CollectionsAndGenerics
     {
         static void Main(string[] args)
         {
+
+            Int32 a = 5;
+            Int32 b = 6;
+            Console.WriteLine("a = {0}, b ={1}", a, b);
+            swap( ref a, ref b);
+            Console.WriteLine("a = {0}, b ={1}",a,b);
+            Console.WriteLine("Уба! Уба!");
+            Console.ReadLine();
+            
+        }
+
+        static void swap<T>(ref T a, ref T b){
+            T temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
